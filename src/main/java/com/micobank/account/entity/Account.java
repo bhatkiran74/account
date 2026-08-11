a2825db518105bb9445160fe5b5a38c31789e9f7
kiransing <bhatkiran74@gmail.com>
2026-08-11 14:15:41 +0530

    implemented account entity and yml configuration for jpa

---
+++ src/main/java/com/micobank/account/entity/Account.java
@@ -0,0 +1,31 @@
+package com.micobank.account.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "account")
+public class Account extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+}
