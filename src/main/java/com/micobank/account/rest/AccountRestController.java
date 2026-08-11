045a310362e3f2a38fe13d1abd13e403cf924ff6
kiransing <bhatkiran74@gmail.com>
2026-08-11 14:39:38 +0530

    implemented fetchAccountDetailsUsingMobileNo

--- src/main/java/com/micobank/account/rest/AccountRestController.java
+++ src/main/java/com/micobank/account/rest/AccountRestController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/account", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -28,5 +25,12 @@ public class AccountRestController {
     }
 
 
+
+    @GetMapping("/fetch")
+    ResponseEntity<CustomerDto> fetchAccountDetailsUsingMobileNo(@RequestParam String mobileNumber){
+
+        CustomerDto customerDto = iAccountService.findAccountDetails(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
 
 }
