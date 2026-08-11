5a033ae570847220a4c6e3b9a1e4dea939443420
kiransing <bhatkiran74@gmail.com>
2026-08-11 14:16:08 +0530

    implemented accountDto, AccountMapper, AccountConstants

---
+++ src/main/java/com/micobank/account/constants/AccountConstants.java
@@ -0,0 +1,19 @@
+package com.micobank.account.constants;
+
+public class AccountConstants {
+
+    private AccountConstants() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "Yashwin Hinjewadi, Phase 2, Pune- 411057";
+    public static final String CURRENT = "Current";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request proceed successfully";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occured. Please try again or contact dev team";
+
+}
